c0a95419319ddc35d91c0cc20789701fdb8293e6
woShiXiaoMaNong <zhongming139@126.com>
2019-06-30 16:56:40 +0800

    设置调色板，色号。使用的是书中的设置

--- zmosII/harib00j/bootpack.c
+++ zmosII/harib00j/bootpack.c
@@ -1,13 +1,20 @@
 
 
 void io_hlt(void);
-void write_mem8(int addr,int data);
+void init_palette(void);
+void set_palette(int color_num_start, int color_num_end, unsigned char *rgb);
+int io_load_eflags(void);
+void io_cli(void);
+void io_store_eflags(int eflags);
+
 
 void HariMain(void)
 {
+	init_palette();
 	int i;
-	for (i = 0xa1000;i<= 0xa100f; i++){
-		write_mem8(i,12);
+	char* p = (char *)0xa0000;
+	for (i = 0;i<= 0xffff; i++){
+		*(p + i) = i & 0x0f; // mov byte [0xa0000 + i], i & 0x0f
 	}
 fin:
 	io_hlt();
@@ -15,3 +22,51 @@ fin:
 
 }
    
+void init_palette(void)
+{
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黒 */
+		0xff, 0x00, 0x00,	/*  1:明るい赤 */
+		0x00, 0xff, 0x00,	/*  2:明るい緑 */
+		0xff, 0xff, 0x00,	/*  3:明るい黄色 */
+		0x00, 0x00, 0xff,	/*  4:明るい青 */
+		0xff, 0x00, 0xff,	/*  5:明るい紫 */
+		0x00, 0xff, 0xff,	/*  6:明るい水色 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:明るい灰色 */
+		0x84, 0x00, 0x00,	/*  9:暗い赤 */
+		0x00, 0x84, 0x00,	/* 10:暗い緑 */
+		0x84, 0x84, 0x00,	/* 11:暗い黄色 */
+		0x00, 0x00, 0x84,	/* 12:暗い青 */
+		0x84, 0x00, 0x84,	/* 13:暗い紫 */
+		0x00, 0x84, 0x84,	/* 14:暗い水色 */
+		0x84, 0x84, 0x84	/* 15:暗い灰色 */
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+
+	/* static char = DB:0x00,0x00,0x00..... */
+}
+
+
+
+void set_palette(int color_num_start, int color_num_end, unsigned char *rgb)
+{
+	int color_mode_port = 0x03c9;
+	int i,eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(color_mode_port,color_num_start);			//调色板号码，只需要开始写入一次，只有就可以连续写入其他调色板的颜色配置。
+	for(i = color_num_start;i <= color_num_end; i++){
+		io_out8(color_mode_port,rgb[0] / 4);
+		io_out8(color_mode_port,rgb[1] / 4);
+		io_out8(color_mode_port,rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflag(eflags);
+}
+
+
+
+
+
--- zmosII/harib00j/naskfunc.nas
+++ zmosII/harib00j/naskfunc.nas
@@ -9,8 +9,7 @@
 
 [FILE "naskfunc.nas"]			; 文件名
 
-		GLOBAL	_io_hlt			; c?言中的函数名，在函数名前加上下??。
-		GLOBAL  _write_mem8
+		GLOBAL	_io_hlt,_io_out8, _io_load_eflags,_io_cli, _io_store_eflag			; c中的函数名，在函数名前加上下??。
 
 ; 以下は実際の関数
 
@@ -20,9 +19,22 @@ _io_hlt:	; void io_hlt(void);
 		HLT
 		RET
 		
-		
-_write_mem8:		;write_mem8(int addr,int data)
-	mov ecx,[esp + 4] ;第一个参数，即addr
-	mov al, [esp + 8] ;第二个参数，即data
-	mov [ecx],al    ;
-	RET
+_io_out8:	;void io_out8(int port,int value);
+		mov edx,[esp + 4]
+		mov eax,[esp + 8]
+		out dx,al
+		ret
+_io_load_eflags:  ;int io_load_eflags(void);
+		pushfd
+		pop eax  ;eax 存放返回?
+		ret
+
+_io_cli:		;void io_cli
+		cli
+		ret
+
+_io_store_eflag:	;void io_store_eflag(int eflags)
+		mov eax,[esp + 4]
+		push eax
+		popfd
+		ret
